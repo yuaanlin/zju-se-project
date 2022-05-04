d2159a84193a387a24dbdc2d4e7136e076e244a5
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-05-04 22:55:51 +0800

    Add a modal in appointment page

--- pages/appointment/index.tsx
+++ pages/appointment/index.tsx
@@ -1,4 +1,8 @@
+import { Button, Modal } from 'antd';
+import { useState } from 'react';
+
 export default function AppointmentPage() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <div>
       <h1>门诊预约/管理平台</h1>
@@ -6,6 +10,15 @@ export default function AppointmentPage() {
         在 <code>pages/appointment</code> 目录下添加 <code>.tsx</code> 文件，
         即可在这个模块的路由下添加需要的二级路由。
       </p>
+      <Button
+        onClick={() => {
+          setIsModalVisible(true);
+        }}>I am a button</Button>
+      <Modal title="Basic Modal" visible={isModalVisible}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </div>
   );
 }
