046129d1f41c275514cc346f732524ae057a59a3
cherylQL <1030821728@qq.com>
2022-05-11 20:39:09 +0800

    [+] Add `Modal Form` logic in ./page/appointment.tsx

--- pages/appointment/index.tsx
+++ pages/appointment/index.tsx
@@ -1,12 +1,16 @@
 import SiderMenu from '../component/SiderMenu';
-import { Table, Tag, Space, Modal, Layout } from 'antd';
+import AddAppointmentModal, { MODAL_STATUS } from '../component/addAppointmentModal';
+import { Button, Form, Layout, Space, Table, Tag } from 'antd';
 import { useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+
 const { Content } = Layout;
 
 function findColor(tag: number) {
   let t = tagColor.find(x=>x.status == tag);
   return t ? t.color : 'green';
 }
+
 const tagColor = [
   {
     status: 0,
@@ -63,6 +67,28 @@ const columns = [
 ];
 
 const data = [
+
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: 0,
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: 1,
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: 2,
+  },
   {
     key: '1',
     name: 'John Brown',
@@ -87,15 +113,14 @@ const data = [
 ];
 export default function AppointmentPage() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [modalStatus, setModalStatus] = useState(MODAL_STATUS.USER_ADD_APPOINTMENT);
+  const [form] = Form.useForm();
   const showModal = () => {
     setModalVisible(true);
   };
 
-  const handleOk = () => {
-    setModalVisible(false);
-  };
-
-  const handleCancel = () => {
+  const onCreate = (values: any) => {
+    console.log('Received values of form: ', values);
     setModalVisible(false);
   };
   return (
@@ -113,21 +138,41 @@ export default function AppointmentPage() {
           <h1>门诊预约/管理平台</h1>
           <Table
             onRow={record => {
-              return { onClick: () => {setModalVisible(true);} };
+              return {
+                onClick: () => {
+                  setModalStatus(MODAL_STATUS.DOCTOR_EDIT_ADD_APPOINTMENT);
+                  setModalVisible(true);
+                }
+              };
+            }}
+            pagination={{
+              position:['bottomCenter'],
+              hideOnSinglePage: true,
+              pageSize: 5
             }}
             columns={columns}
             dataSource={data}
           />
-          <Modal
-            title="Basic Modal"
+          <AddAppointmentModal
+            modalStatus={modalStatus}
             visible={modalVisible}
-            onOk={handleOk}
-            onCancel={handleCancel}>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-          </Modal>
+            onCreate={onCreate}
+            onCancel={()=>{setModalVisible(false);}}
+          />
         </div>
+        <Button
+          style={{
+            position: 'absolute',
+            right: 100,
+            bottom: 150
+          }}
+          onClick={()=>{
+            setModalStatus(MODAL_STATUS.USER_ADD_APPOINTMENT);
+            setModalVisible(true);
+          }}
+          type="primary"
+          shape="circle"
+          icon={<PlusOutlined />} />
       </Content>
     </>
   );
