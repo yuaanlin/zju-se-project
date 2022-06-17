af8a804d9a96095bacdf98c05068503ddac93873
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-06-17 09:53:37 +0800

    Trying to setup rewrites for dev

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://116.62.230.9:4000/api/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
